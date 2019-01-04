f4fd922a4b0c106d41cf42073726b6e55e1057c8
mincj19 <mincj19@BFHS-382228.pwcs.edu>
2019-01-04 13:15:07 -0500

    Added Project

---
+++ groupProject/confirmedGroupProject/personA.java
@@ -0,0 +1,14 @@
+package confirmedGroupProject;
+
+public class Test {
+	
+	public static void main ( String [] args ) {
+		avg( "hello" , 15 );
+	}
+	
+	public static void avg( Integer a , String b ) {
+		return a + b;
+	}
+
+}
+
