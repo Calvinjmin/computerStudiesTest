448ac88d926cc2bf21dc6c71e236e72f56781d20
Calvin Min <MinCJ19@pwcs-edu.org>
2019-01-04 12:56:11 -0500

    Person A

--- groupProject/test.java
+++ groupProject/test.java
@@ -1,3 +1,7 @@
+//Person A
+//Create a simple Java project in Eclipse. Create a single source file that has a main method. In the main method, ask someone to type in two integers, 
+//calculate the average, the print the average. Make several simple mistakes in your program.
+
 public class Test {
 	
 	public static void main ( String [] args ) {
