f076aaadd7886e131b25b2bc1409236f2075481a
mincj19 <mincj19@BFHS-382228.pwcs.edu>
2019-01-04 13:38:44 -0500

    Comment

--- confirmedGroupProject/personC.java
+++ confirmedGroupProject/personC.java
@@ -1,5 +1,10 @@
 package confirmedGroupProject;
 
 public class personC extends personA {
-
+	public int randoRange( int high, int low, boolean idk)
+	{
+		int i = 0;
+		i = high * low /4 + 6;
+		return i;
+	}
 }
