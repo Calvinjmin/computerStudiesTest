8828d3a428c14df85b3546685ef5ce2233579a30
Calvin Min <MinCJ19@pwcs-edu.org>
2019-01-04 13:56:52 -0500

    hello

--- confirmedGroupProject/personA.java
+++ confirmedGroupProject/personA.java
@@ -1,5 +1,12 @@
 package confirmedGroupProject;
 
 public class personA {
+	public static void main( String [] args ) {
+		int average = avg( "hello" , 10 );
+	}
+	
+	public void avg( Integer a , String b ) {
+		return a + b;
+	}
 
 }
